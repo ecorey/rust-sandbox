9cc869ad2fd3b6898282bfa1fd37ec8d3cdac86a
ecorey <bengstrom32@gmail.com>
2024-08-24 16:59:19 -0400

    commit

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,16 @@
 fn main() {
     println!("Hello, world!");
 }
+
+// create a function that takes a vector of u8 and returns a string
+fn vec_to_string(v: Vec<u8>) -> String {
+    // create a new string
+    let mut s = String::new();
+    // iterate over the vector
+    for i in v {
+        // push the character to the string
+        s.push(i as char);
+    }
+    // return the string
+    s
+}
